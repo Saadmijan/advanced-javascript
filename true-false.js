e1bafab49b5244402a056501b6816602fcd0c256
Saadmijan <mijanmaeen@gmail.com>
2020-03-20 00:15:35 -0700

    true-false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//Falsy;
+//False
+// 0
+// ""
+// undefined
+// null
+// Nan
+//Truthy
+//'0'.' ' []
+let name = 0;
+
+if(name || name ==0){
+    console.log("condition is true");
+
+}
+
+else {
+    console.log("condition is false");
+}
